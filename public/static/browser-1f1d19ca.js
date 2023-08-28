63da0cc7b21b498caf4351ebe6888636d8c41806
Jayant Rohila <jrohila55@gmail.com>
2023-08-28 20:47:38 +0530

    update

--- public/static/browser-1f1d19ca.js
+++ public/static/browser-1f1d19ca.js
@@ -1,4 +1,4 @@
-import{av as Xe,aw as qe}from"./sanity-8ef79b21.js";function Ue(G,Q){for(var y=0;y<Q.length;y++){const T=Q[y];if(typeof T!="string"&&!Array.isArray(T)){for(const _ in T)if(_!=="default"&&!(_ in G)){const h=Object.getOwnPropertyDescriptor(T,_);h&&Object.defineProperty(G,_,h.get?h:{enumerable:!0,get:()=>T[_]})}}}return Object.freeze(Object.defineProperty(G,Symbol.toStringTag,{value:"Module"}))}var de={exports:{}};/** @license
+import{av as Xe,aw as qe}from"./sanity-5f6ecca4.js";function Ue(G,Q){for(var y=0;y<Q.length;y++){const T=Q[y];if(typeof T!="string"&&!Array.isArray(T)){for(const _ in T)if(_!=="default"&&!(_ in G)){const h=Object.getOwnPropertyDescriptor(T,_);h&&Object.defineProperty(G,_,h.get?h:{enumerable:!0,get:()=>T[_]})}}}return Object.freeze(Object.defineProperty(G,Symbol.toStringTag,{value:"Module"}))}var de={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
---
+++ public/static/index-a59da7d1-c725696b.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-990e561a-3af73a34.js";import{D as m}from"./sanity-5f6ecca4.js";import"./index-b305a918.js";export{m as DocumentPaneProvider,t as default};
